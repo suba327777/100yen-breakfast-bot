0b9c57a21d018a7441d1ffdc68d54df0de3e9f6c
suba <84484832+suba327777@users.noreply.github.com>
2023-04-25 01:21:20 +0900

    clean : this file

--- discord-bot/src/commands/calendar.rs
+++ discord-bot/src/commands/calendar.rs
@@ -1,5 +1,4 @@
-use crate::google::event::{fetch_schedule, CalendarEvent};
-use crate::utils::format::date;
+use crate::google::event::fetch_schedule;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
@@ -7,15 +6,10 @@ use serenity::prelude::*;
 #[command]
 #[description = "fetch schedule week"]
 async fn sch(ctx: &Context, msg: &Message) -> CommandResult {
-    let event: CalendarEvent = fetch_schedule().await;
-
-    let event_message = date(event);
+    let event_message: String = fetch_schedule().await;
 
     msg.channel_id
-        .say(
-            &ctx.http,
-            format!("予定されている日程はこちらになるよ！\n{}", event_message),
-        )
+        .say(&ctx.http, format!("{}", event_message))
         .await?;
 
     Ok(())
