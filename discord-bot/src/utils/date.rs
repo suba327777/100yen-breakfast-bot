7ed1328702ef2aaf93bee74373f87851146ccbb0
suba <84484832+suba327777@users.noreply.github.com>
2023-04-25 01:18:21 +0900

    refactor : function name

--- discord-bot/src/utils/date.rs
+++ discord-bot/src/utils/date.rs
@@ -1,7 +1,7 @@
 use chrono::{DateTime, TimeZone, Utc};
 use chrono_tz::{Asia::Tokyo, Tz};
 
-pub fn jst_date_now() -> DateTime<Tz> {
+pub fn date_now_jst() -> DateTime<Tz> {
     let utc = Utc::now().naive_utc();
     let jst = Tokyo.from_utc_datetime(&utc);
 
