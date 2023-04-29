682c8eb85f668f29608fd7fd979c8d4182660e61
suba <84484832+suba327777@users.noreply.github.com>
2023-04-29 16:34:22 +0900

    add:confirm the existence of role once created

--- discord-bot/src/commands/role.rs
+++ discord-bot/src/commands/role.rs
@@ -6,17 +6,26 @@ use serenity::prelude::*;
 #[description("create a role")]
 async fn create_role(ctx: &Context, msg: &Message) -> CommandResult {
     let guild = msg.guild(&ctx.cache).unwrap();
-    // let roles = guild
-    //     .roles
-    //     .value
-    // println!("{}", response);
-    let role = guild
-        .create_role(&ctx.http, |r| r.name("100円朝食"))
-        .await
-        .unwrap();
-    msg.channel_id
-        .say(&ctx.http, format!("Created role {}", role.name))
-        .await?;
-    println!("Created role {}", &role.name);
-    Ok(())
+    let is_roles = guild.roles.values().any(|r| r.name == "100円朝食");
+    if is_roles {
+        msg.channel_id
+            .say(&ctx.http, "ロールはすでに存在してますよ〜")
+            .await?;
+        println!("Role already exists");
+        Ok(())
+    } else {
+        let role = guild
+            .create_role(&ctx.http, |r| r.name("100円朝食").colour(8210593))
+            .await
+            .unwrap();
+
+        msg.channel_id
+            .say(
+                &ctx.http,
+                format!("{}というロールを作成したよ！", role.name),
+            )
+            .await?;
+        println!("Created role {}", &role.name);
+        Ok(())
+    }
 }
